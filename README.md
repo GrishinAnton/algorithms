054b9b013618fb6068434aa327c130147cde1087
GrishinAnton <asegrishin@sberbank.ru>
2020-04-24 22:46:49 +0300

    to_lower_case

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # algorithms
 Задачи по алгоритмам и структурам данных.
 2. https://leetcode.com/problems/defanging-an-ip-address/ - defanging_an_ip_address
+3. https://leetcode.com/problems/to-lower-case/ - to_lower_case
---
+++ to_lower_case.js
@@ -0,0 +1,26 @@
+/**
+    Input: "Hello"
+    Output: "hello"
+    Input: "here"
+    Output: "here"
+    Input: "LOVELY"
+    Output: "lovely"
+    65-90 - A-Z
+    97-122 a-z
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function (str) {
+    let result = ''
+    for (let i = 0; i < str.length; i++) {
+        let current = str[i].charCodeAt()
+        if (current >= 65 && current <= 90) {
+            result += String.fromCharCode(current + 32)
+            continue
+        }
+        result += str[i]
+    }
+    return result
+};
+
+console.log(toLowerCase("Hello"))
