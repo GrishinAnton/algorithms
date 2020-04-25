00ff0ad1b6bbd28c88562310da612cd7a1285614
GrishinAnton <asegrishin@sberbank.ru>
2020-04-25 08:11:23 +0300

    Решение через цикл

---
+++ jewels_and_Stones.js
@@ -0,0 +1,21 @@
+/**
+    Input: J = "aA", S = "aAAbbbb"
+    Output: 3
+    Input: J = "z", S = "ZZ"
+    Output: 0
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+/** Решение цикл в цикле - сложность N * N */
+var numJewelsInStones = function (J, S) {
+    let result = 0
+    for (let letterJ in J) {
+        for (let letterS in S) {
+            if (J[letterJ] === S[letterS]) result++
+        }
+    }
+    return result
+};
+
+console.log(numJewelsInStones("aA", "aAAbbbb"))
