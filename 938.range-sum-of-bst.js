246305a40b6de5e33fe8f7a53bf57a8c855187a6
GrishinAnton <asegrishin@sberbank.ru>
2020-04-27 16:23:40 +0300

    Старт задачи

---
+++ 938.range-sum-of-bst.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=938 lang=javascript
+ *
+ * [938] Range Sum of BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+var rangeSumBST = function(root, L, R) {
+    
+};
+// @lc code=end
+
