2796b3a3de9f464daeb549244ec5953f61bc0ea3
GrishinAnton <asegrishin@sberbank.ru>
2020-04-27 16:34:06 +0300

    Старт задачи

---
+++ 804.unique-morse-code-words.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=804 lang=javascript
+ *
+ * [804] Unique Morse Code Words
+ */
+/**
+ * 804. Unique Morse Code Words
+ * @namespace LEETCODE
+ * @link https://leetcode.com/problems/unique-morse-code-words
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function (words) {
+
+};
+// @lc code=end
+
+/**
+ * Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation:
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+ */
+
