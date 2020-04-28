1a10a187f5ed63522ef5410df653339ba320d6d5
GrishinAnton <asegrishin@sberbank.ru>
2020-04-28 08:03:29 +0300

    Сериализация

--- 617.merge-two-binary-trees.js
+++ 617.merge-two-binary-trees.js
@@ -33,22 +33,51 @@ function summNode(first, second) {
     return first === null ? second : first
 }
 
+function bfs(root) {
+    const q = [root];
+    const result = [];
+    const fakeNode = {
+        val: null,
+        left: null,
+        right: null
+    }
+
+    while (q.length > 0) {
+        const front = q.shift();
+        result.push(front.val);
+        if (front.left === null && front.right === null) {
+            continue
+        }
+        if (front.left === null && front.right !== null) {
+            q.push(fakeNode)
+            q.push(front.right);
+            continue
+        }
+
+        q.push(front.left);
+        front.right && q.push(front.right);
+    }
+    return result
+}
+
 var mergeTrees = function (t1, t2) {
     let result = []
+
     let maxArrLength = []
     let minArrLength = []
 
     if (t1.length >= t2.length) {
-        maxArrLength = t1
-        minArrLength = t2
+        maxArrLength = bfs(t1)
+        minArrLength = bfs(t2)
     } else {
-        maxArrLength = t2
-        minArrLength = t1
+        maxArrLength = bfs(t2)
+        minArrLength = bfs(t1)
     }
 
     for (let i = 0; i < maxArrLength.length; i++) {
         result.push(summNode(maxArrLength[i], minArrLength[i]))
     }
+
     return result
 };
 // console.log(mergeTrees([1, 3, 2, 5], [2, 1, 3, null, 4, null, 7]), 'erturn')
