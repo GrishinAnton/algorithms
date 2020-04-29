fa82671787e211a4ece3b86cc2750a9b63dde41c
GrishinAnton <asegrishin@sberbank.ru>
2020-04-29 08:15:17 +0300

    Решение

--- 1221.split-a-string-in-balanced-strings.js
+++ 1221.split-a-string-in-balanced-strings.js
@@ -9,8 +9,18 @@
  * @param {string} s
  * @return {number}
  */
+//Сложность  O(n)
 var balancedStringSplit = function (s) {
+    let result = 0
+    let count = 0
 
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "R") count++
+        if (s[i] === "L") count--
+        if (count === 0) result++
+
+    }
+    return result
 };
 // @lc code=end
 
