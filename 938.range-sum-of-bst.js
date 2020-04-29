d7ea24a3e680a56872b39e6f3108951f4cc8d783
GrishinAnton <asegrishin@sberbank.ru>
2020-04-29 07:53:39 +0300

    Решение с отрезанием веток, которые изначально не подходят.

--- 938.range-sum-of-bst.js
+++ 938.range-sum-of-bst.js
@@ -23,52 +23,41 @@
  * @param {number} R
  * @return {number}
  */
-// let tt = {
-
-//     val: 10,
-
-//     left:
-
-//     {
-
-//         val: 5,
-
-//         left: { val: 3, left: { val: 1, left: null, right: null }, right: null },
-
-//         right: { val: 7, left: { val: 6, left: null, right: null }, right: null }
-//     },
-
-//     right:
-
-//     {
-
-//         val: 15,
-
-//         left: { val: 13, left: null, right: null },
-
-//         right: { val: 18, left: null, right: null }
-//     }
-// }
+let tt = {
+    val: 10,
+    left: {
+        val: 5,
+        left: { val: 3, left: { val: 1, left: null, right: null }, right: null },
+        right: { val: 7, left: { val: 6, left: { val: 5, left: null, right: null }, right: null }, right: null }
+    },
+    right: {
+        val: 15,
+        left: { val: 13, left: null, right: null },
+        right: { val: 18, left: null, right: null }
+    }
+}
 
 /**
  * Сложность O(n) где n кол-во нод
  */
 var rangeSumBST = function (root, L, R, ) {
+    debugger
     let result = 0
 
-    if (root === null) {
-        return 0
-    }
-
+    if (root === null) return 0
     if (root.val >= L && root.val <= R) {
         result += root.val
-    }
 
-    result += rangeSumBST(root.left, L, R)
-    result += rangeSumBST(root.right, L, R)
+    }
+    if (root.val > L && root.left !== null) {
+        result += rangeSumBST(root.left, L, R)
+    }
+    if (root.val < R && root.right !== null) {
+        result += rangeSumBST(root.right, L, R)
+    }
 
     return result
 };
-// console.log(rangeSumBST(tt, 6, 10));//23
+console.log(rangeSumBST(tt, 6, 10));//23
 // @lc code=end
 
