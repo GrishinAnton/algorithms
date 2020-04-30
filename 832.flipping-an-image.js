90dc13e8ab24269e1f747923d4178080ca8e98d5
GrishinAnton <asegrishin@sberbank.ru>
2020-04-30 08:58:40 +0300

    Поменял на XOR

--- 832.flipping-an-image.js
+++ 832.flipping-an-image.js
@@ -18,11 +18,11 @@
 var flipAndInvertImage = function (A) {
 
     return A.map(item => {
-        return item.reverse().map(number => number === 0 ? 1 : 0)
+        return item.reverse().map(number => number ^= 1)
     })
 
 };
-console.log(flipAndInvertImage([[1, 1, 0], [1, 0, 1], [0, 0, 0]]))
+// console.log(flipAndInvertImage([[1, 1, 0], [1, 0, 1], [0, 0, 0]]))
 
 // @lc code=end
 
