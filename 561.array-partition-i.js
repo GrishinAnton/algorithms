cbecc408e12f37e3c0b890293064d4f318821af9
GrishinAnton <asegrishin@sberbank.ru>
2020-04-30 08:37:01 +0300

    Решение на память

--- 561.array-partition-i.js
+++ 561.array-partition-i.js
@@ -25,6 +25,45 @@ var arrayPairSum = function (nums) {
     return result
 
 };
+/**
+
+[
+1,2,3,4,5,6,4,4,4,4
+]
+          1
+         (1)
+          1
+         (1)
+(1),1,(1),1,(1),1
+
+100001 - 10000 = 1
+100003 - 10000 = 3
+100004 - 10000 = 4
+100004 - 10000 = 4
+100005 - 10000 = 5
+               = 17
+var arrayPairSum = function (nums) {
+    debugger
+    let exist = []
+    for (let i = 0; i < nums.length; i++) {
+        exist[nums[i] + 10] = (exist[nums[i] + 10] || 0) + 1
+    }
+
+    let odd = true // flag to control add or not
+    let sum = 0
+    for (let i = 0; i < exist.length; i++) {
+        while (exist[i]) {
+            if (odd) {
+                sum += i - 10
+            }
+            odd = !odd
+            exist[i]-- // after met, -1
+        }
+    }
+    return sum
+
+};
+*/
 // @lc code=end
 
 /**
