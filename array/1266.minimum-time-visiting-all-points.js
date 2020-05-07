4cc45d13c4b6e04afa7a6113e7a54f75395d38f3
GrishinAnton <asegrishin@sberbank.ru>
2020-05-07 07:29:02 +0300

    Инит

---
+++ array/1266.minimum-time-visiting-all-points.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1266 lang=javascript
+ *
+ * [1266] Minimum Time Visiting All Points
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    
+};
+// @lc code=end
+
