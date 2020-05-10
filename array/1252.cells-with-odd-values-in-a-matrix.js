eaa8a28ea53e7880a6cccf4be727e71450fb381e
GrishinAnton <asegrishin@sberbank.ru>
2020-05-10 15:13:06 +0300

    решение

--- array/1252.cells-with-odd-values-in-a-matrix.js
+++ array/1252.cells-with-odd-values-in-a-matrix.js
@@ -11,7 +11,36 @@
  * @param {number[][]} indices
  * @return {number}
  */
+//Сложность  О(n * m)
 var oddCells = function (n, m, indices) {
+
+    let matrix = []
+    let result = 0
+
+    for (let i = 0; i < n; i++) {
+        matrix.push(new Array(m).fill(0))
+    }
+
+    for (let j = 0; j < indices.length; j++) {
+        let [row, cell] = indices[j]
+
+        matrix[row].forEach((item, index) => matrix[row][index]++)
+        matrix.forEach(item => {
+            item[cell]++
+        })
+
+
+
+    }
+    matrix.forEach(item => {
+        item.forEach(number => {
+            if (number % 2 !== 0) {
+                result++
+            }
+        })
+    })
+
+    return result
 
 };
 // @lc code=end
