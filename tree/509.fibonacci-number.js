c6099ff7f31e1194c1dd9b29b03de4abdd19fb6d
GrishinAnton <asegrishin@sberbank.ru>
2020-05-10 17:42:05 +0300

    Инит

---
+++ tree/509.fibonacci-number.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=509 lang=javascript
+ *
+ * [509] Fibonacci Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var fib = function(N) {
+    
+};
+// @lc code=end
+
