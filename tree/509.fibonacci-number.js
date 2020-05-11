85fb053a7e4fe1b5ac4942d75a022b6fb9b017d6
GrishinAnton <asegrishin@sberbank.ru>
2020-05-11 14:24:20 +0300

    Начало работы с циклом

--- tree/509.fibonacci-number.js
+++ tree/509.fibonacci-number.js
@@ -9,10 +9,31 @@
  * @param {number} N
  * @return {number}
  */
+/**
+ * 2= 1+0 = 1
+ * 3= 1+1 =2
+ * 4= 1+1 =2, 2+1=3
+ * 5= 1+1 =2, 2+1=3, 3+2 =5
+ * 6= 1+1 =2, 2+1=3, 3+2 =5, 5+8=13
+ * 7= 1+1 =2, 2+1=3, 3+2 =5, 5+8=13, 13+8=21
+ * 8= 1+1 =2, 2+1=3, 3+2 =5, 5+8=13, 13+8=21, 21+13=34
+ */
+//Итеративно
 var fib = function (N) {
-    if (N === 0) return 0
-    return N > 2 ? fib(N - 1) + fib(N - 2) : 1
-};
+    let count = 1
+    let result = 1
+
+    for (let i = 2; i < N; i++) {
+
+        result += count
+    }
+    return result
+}
+//Рекурсивно
+// var fib = function (N) {
+//     if (N === 0) return 0
+//     return N > 2 ? fib(N - 1) + fib(N - 2) : 1
+// };
 
 // @lc code=end
 
