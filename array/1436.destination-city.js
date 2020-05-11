8f4d8a9b04948c9fc0bef528ea5395c8803e6bb7
GrishinAnton <asegrishin@sberbank.ru>
2020-05-11 09:23:22 +0300

    Инит

---
+++ array/1436.destination-city.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+var destCity = function (paths) {
+
+};
+
+console.log(destCity([["London", "New York"], ["New York", "Lima"], ["Lima", "Sao Paulo"]]));
+// console.log(destCity([["B", "C"], ["D", "B"], ["C", "A"]]))
+// console.log(destCity([["A", "Z"]]))
