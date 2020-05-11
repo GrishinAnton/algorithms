061ff17a26b8114de11eada74705f856423d9530
GrishinAnton <asegrishin@sberbank.ru>
2020-05-11 08:32:03 +0300

    Решение

--- tree/509.fibonacci-number.js
+++ tree/509.fibonacci-number.js
@@ -1,4 +1,4 @@
-/*
+/**
  * @lc app=leetcode id=509 lang=javascript
  *
  * [509] Fibonacci Number
@@ -9,8 +9,10 @@
  * @param {number} N
  * @return {number}
  */
-var fib = function(N) {
-    
+var fib = function (N) {
+    if (N === 0) return 0
+    return N > 2 ? fib(N - 1) + fib(N - 2) : 1
 };
+
 // @lc code=end
 
