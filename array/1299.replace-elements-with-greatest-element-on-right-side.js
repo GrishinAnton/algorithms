641c35b011cdd3e694396df4e3c7435f26dd68c0
GrishinAnton <asegrishin@sberbank.ru>
2020-05-11 16:18:14 +0300

    Инит

---
+++ array/1299.replace-elements-with-greatest-element-on-right-side.js
@@ -0,0 +1,16 @@
+/**
+ * @lc app=leetcode id=1299 lang=javascript
+ *
+ * [1299] Replace Elements with Greatest Element on Right Side
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function (arr) {
+
+};
+// @lc code=end
+
