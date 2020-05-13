57aea12245adb146ec6a1593a1b1cec1375cdfd8
GrishinAnton <asegrishin@sberbank.ru>
2020-05-13 06:45:58 +0300

    Обновил решение

--- array/1252.cells-with-odd-values-in-a-matrix.js
+++ array/1252.cells-with-odd-values-in-a-matrix.js
@@ -11,34 +11,59 @@
  * @param {number[][]} indices
  * @return {number}
  */
+// //Сложность  О(n * m)
+// var oddCells = function (n, m, indices) {
+
+//     let matrix = []
+//     let result = 0
+
+//     for (let i = 0; i < n; i++) {
+//         matrix.push(new Array(m).fill(0))
+//     }
+
+//     for (let j = 0; j < indices.length; j++) {
+//         let [row, cell] = indices[j]
+
+//         matrix[row].forEach((item, index) => matrix[row][index]++)
+//         matrix.forEach(item => {
+//             item[cell]++
+//         })
+
+
+
+//     }
+//     matrix.forEach(item => {
+//         item.forEach(number => {
+//             if (number % 2 !== 0) {
+//                 result++
+//             }
+//         })
+//     })
+
+//     return result
+
+// };
 //Сложность  О(n * m)
 var oddCells = function (n, m, indices) {
 
-    let matrix = []
     let result = 0
+    let row = new Array(n).fill(0)
+    let cols = new Array(m).fill(0)
 
-    for (let i = 0; i < n; i++) {
-        matrix.push(new Array(m).fill(0))
+    for (let i = 0; i < indices.length; i++) {
+        let [r, c] = indices[i]
+        row[r]++
+        cols[c]++
     }
 
-    for (let j = 0; j < indices.length; j++) {
-        let [row, cell] = indices[j]
-
-        matrix[row].forEach((item, index) => matrix[row][index]++)
-        matrix.forEach(item => {
-            item[cell]++
-        })
-
-
-
-    }
-    matrix.forEach(item => {
-        item.forEach(number => {
-            if (number % 2 !== 0) {
+    row.forEach(rowNumber => {
+        cols.forEach(colsNumber => {
+            if ((colsNumber + rowNumber) % 2 !== 0) {
                 result++
             }
         })
     })
+
 
     return result
 
