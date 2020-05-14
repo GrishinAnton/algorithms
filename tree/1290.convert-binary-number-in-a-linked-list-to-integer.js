488e0ee1a3227c20d4f206050db79d146f08169a
GrishinAnton <asegrishin@sberbank.ru>
2020-05-14 21:26:17 +0300

    Решение

--- tree/1290.convert-binary-number-in-a-linked-list-to-integer.js
+++ tree/1290.convert-binary-number-in-a-linked-list-to-integer.js
@@ -19,11 +19,42 @@
 //Сложность O(n) по поамяти O(n)
 // 0 x 2 + i = symm
 // summ X 2 + i = summ
-var getDecimalValue = function (head) {
-    if (!head) return null
-    if (!head.next) return head.val
+// var getDecimalValue = function (head) {
+//     let summ = 0
 
-    return head.val * (2 + getDecimalValue(head.next))
+//     const getDecimalValueRec = root => {
+//         if (!root) return null
+//         let tmp = summ * 2 + root.val
+
+//         summ = tmp
+//         getDecimalValueRec(root.next)
+//     }
+
+//     getDecimalValueRec(head)
+
+//     return summ
+// };
+
+//Вариант через возведение в степерь числа
+//  0   1   0   1
+// 2:3 2:2 2:1 2:0
+var getDecimalValue = function (head, height = 0) {
+    if (!head) return null
+
+    let heightRec = height || getHeight(head) - 1
+
+    return head.val
+        ? 2 ** heightRec + getDecimalValue(head.next, heightRec - 1)
+        : getDecimalValue(head.next, heightRec - 1)
 };
+
+function getHeight(head) {
+    if (!head) return 0
+
+    return 1 + getHeight(head.next)
+}
+
+
+
 // @lc code=end
 
