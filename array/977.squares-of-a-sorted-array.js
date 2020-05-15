46f2bb2cbc6c8c58044187bce42f0a44262f32b3
GrishinAnton <asegrishin@sberbank.ru>
2020-05-15 08:36:14 +0300

    В процессе

--- array/977.squares-of-a-sorted-array.js
+++ array/977.squares-of-a-sorted-array.js
@@ -9,45 +9,6 @@
  * @param {number[]} A
  * @return {number[]}
  */
-var sortedSquares = function (A) {
-    if (A.length === 1) return [Math.abs(A[0])]
-    let result = []
-    let minNumber = -10001
-    let minIndexArr = null
-
-
-    for (let i = 0; i < A.length; i++) {
-        if (Math.min(Math.abs(A[i]), Math.abs(minNumber)) === A[i]) {
-            minNumber = A[i]
-            minIndexArr = i
-        }
-
-        A[i] = Math.abs(A[i]) ** 2
-    }
-
-    console.log(minIndexArr);
-
-
-    let min = minIndexArr - 1
-    let max = minIndexArr + 1
-    result.push(A[minIndexArr])
-
-    while (true) {
-
-        let dec = A[min] || -1
-        let inc = A[max] || 10001
-
-        min >= 0 && result.push(Math.min(dec, inc))
-        max <= A.length - 1 && result.push(Math.max(dec, inc))
-
-        if (min <= 0 && max >= A.length - 1) break
-
-        min--
-        max++
-    }
-
-    return result
-};
 // var sortedSquares = function (A) {
 //     let hashmap = []
 //     let result = []
@@ -68,5 +29,42 @@ var sortedSquares = function (A) {
 //     }
 //     return result
 // };
+function findIndexOfMin(arr) {
+    let minNumber = -10001
+    let minIndex = null
+
+    for (let i = 0; i < arr.length; i++) {
+        if (Math.min(Math.abs(arr[i]), Math.abs(minNumber)) === Math.abs(arr[i])) {
+            minNumber = arr[i]
+            minIndex = i
+        }
+    }
+
+    return minIndex
+}
+var sortedSquares = function (A) {
+    if (A.length === 1) return [Math.abs(A[0])]
+
+    let result = []
+    const minIndex = findIndexOfMin(A)
+
+    let min = minIndex
+    let max = minIndex + 1 > A.length - 1 ? A.length - 1 : minIndex + 1
+
+    while (min > 0 || max < A.length - 1) {
+
+        const minSqr = A[min] ** 2
+        const maxSqr = A[max] ** 2
+
+        min >= 0 && result.push(Math.min(minSqr, maxSqr))
+        max <= A.length - 1 && result.push(Math.max(minSqr, maxSqr))
+
+        min > 0 && min--
+        max < A.length - 1 && max++
+    }
+
+    return result
+};
+
 // @lc code=end
 
